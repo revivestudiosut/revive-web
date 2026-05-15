a9048d65f354a12fbfda41c3e544fe27d718f013
Trent Davies <trent.davies@gmail.com>
2026-05-15 07:22:41 -0600

    resolve package deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
-export default defineConfig({
-  integrations: [tailwind()],
-});
+export default defineConfig({});
